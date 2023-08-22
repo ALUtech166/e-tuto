c5f9da0346c36cc8c36136d840082b91b227b5f0
ALUtech166 <s.tassiga@alustudent.com>
2023-08-22 21:15:18 +0200

    contact-us

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,34 +1,22 @@
 <template>
   <div class="">
 
-    <div class="tw-relative tw-w-full tw-h-72">
-      <img class="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-full tw-object-cover"
-        src="https://img.freepik.com/free-photo/top-view-workspace-with-notebook-succulent-plant_23-2148430820.jpg?w=740&t=st=1690220651~exp=1690221251~hmac=a7249088cdb9fbe455226feccdb5ea68d2bd60b9c7ae35f497131c6e9a06e731"
-        alt="Banner Image">
-      <div
-        class="tw-relative tw-w-full tw-h-full tw-flex tw-flex-col tw-justify-center tw-items-center tw-bg-gray-900 tw-bg-opacity-60">
-        <h1 class="tw-text-4xl tw-font-bold tw-text-white" data-aos="fade-up">Contact Us</h1>
-      </div>
-    </div>
-    <div class="tw-flex tw-items-center tw-space-x-2 tw-px-8">
-      <v-breadcrumbs :items="breadcrumbItems" divider="/" class="tw-text-lg tw-font-extrabold"></v-breadcrumbs>
+    <div class="tw-mt-4 lg:tw-mx-0 tw-aspect-w-16  tw-overflow-hidden tw-h-full" data-aos="fade-up">
+
+      <iframe class=""
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63800.26059361784!2d30.057428788649723!3d-1.94642533671203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca5a86d814c61%3A0x7d3b83e12b1c11a9!2sNorrsken%20House%20Kigali!5e0!3m2!1sfr!2srw!4v1684802051331!5m2!1sfr!2srw"
+        width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
 
 
-    <div class="lg:tw-mx-8 tw-transform tw--translate-y-58 tw-bottom-0 tw-left-0 tw-px-8 tw-py-12">
+
+    <div class="lg:tw-mx-8 tw-bottom-0 tw-left-0 tw-px-8 tw-py-12">
       <div class="lg:tw-flex lg:tw-flex-row sm:tw-flex sm:tw-flex-col lg:tw-justify-betwen lg:tw-gap-12">
 
-        <div class="tw-text-lg sm:tw-text-lg lg:tw-w-3/4 tw-mb-12">
-          <h1 class="tw-text-black tw-text-8xl tw-font-extrabold">
-            Get in <span class="text-color">touch.</span>
-          </h1>
-          <p class="tw-text-black tw-text-xl tw-mt-2">
-            Reach out to me anytime and I will get back to you within 24 hours.
-          </p>
+        <div class="tw-text-lg tw-transform tw--translate-y-40 sm:tw-text-lg lg:tw-w-1/2 tw-mb-12">
           <div class="">
-            <form
-              class="tw-mt-2 tw-bg-white lg:tw-mt-0 tw-transform tw-translate-y-20 tw-shadow-2xl tw-rounded-xl tw-px-8 tw-py-16"
+            <form class="tw-bg-white lg:tw-mt-0 tw-shadow-2xl tw-rounded-xl tw-px-8 tw-py-12"
               v-on:submit.prevent="sendMessage">
 
               <div class="tw-w-full tw-md:w-1/2">
@@ -108,36 +96,110 @@
               </button>
             </form>
 
-           
+
           </div>
         </div>
 
-        <div class="lg:tw-mt-56 lg:tw-w-1/2 tw-items-center">
-          <img class="tw-rounded-lg tw-shadow-2xl tw-shadow-gray-900 tw-mb-8" src="https://img.freepik.com/free-photo/top-view-workspace-with-notebook-succulent-plant_23-2148430820.jpg?w=740&t=st=1690220651~exp=1690221251~hmac=a7249088cdb9fbe455226feccdb5ea68d2bd60b9c7ae35f497131c6e9a06e731">
+        <div class="lg:tw-mt-8 lg:tw-w-1/2 tw-items-center">
+          <h1 class="tw-text-black tw-text-5xl tw-font-extrabold">
+            How can <span class="text-color">we help?</span>
+          </h1>
+          <p class="tw-text-black tw-text-xl tw-mt-2">
+            Reach out to me anytime and I will get back to you within 24 hours.
+          </p>
 
-             <div class="tw-shadow-2xl tw-rounded-lg tw-shadow-gray-900 tw-p-4">
-              <h1 class="tw-text-black tw-font-bold tw-text-xl tw-mt-2">Shoot me a mail:</h1>
-              <p class="text-color tw-text-2xl tw-font-extrabold tw-mt-4">sergetassiga22@gmail.com</p>
+          <div class="tw-mt-4">
+            <section class="tw-bg-gray-100 tw-shadow-2xl tw-rounded-lg tw-py-8">
+              <div class="tw-container">
+                <h2 class="tw-text-3xl tw-font-extrabold tw-mb-6">Frequently Asked Questions</h2>
+                <div class="tw-space-y-6">
+                  <div v-for="(item, index) in faqItems" :key="index" class="tw-border-b tw-border-gray-300 tw-pb-4">
+                    <div class="tw-flex tw-justify-between tw-items-center">
+                      <h3 class="tw-text-md text-color tw-font-bold">{{ item.question }}</h3>
+                      <button @click="toggleAnswer(index)"
+                        class="tw-text-gray-600 tw-hover:text-gray-800 focus:tw-outline-none">
+                        {{ item.expanded ? 'Collapse' : 'Expand' }}
+                      </button>
+                    </div>
+                    <p v-show="item.expanded" class="tw-text-gray-600 tw-mt-2">{{ item.answer }}</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
 
-              <h2 class="tw-text-black tw-font-bold tw-text-xl tw-mt-4">
-                You can always reach out by calling:</h2>
-              <p class="text-color tw-text-2xl tw-font-extrabold tw-mt-4">+250 785 658 174
-              </p>
-             </div>
-            </div>
+
+
+
+        </div>
 
 
 
       </div> <!-- end hero -->
 
+
+
+      <div class="tw-grid lg:tw-grid-cols-3 tw-gap-6 lg:tw-gap-8 md:tw-gap-8">
+        <div class="tw-shadow-2xl tw-shadow-gray-800 tw-rounded-lg bg-color tw-block tw-cursor-pointer tw-w-full">
+
+          <div class="tw-px-12 tw-p-4 tw-text-left tw-h-[fit-content] lg:tw-h-full lg:tw-my-8">
+
+            <h1 class="tw-font-extrabold tw-text-xl tw-text-white tw-pt-2">
+              Mobile Application
+            </h1>
+            <div class="tw-mt-8">
+              <p class="tw-font-medium tw-text-md tw-text-white tw-text-justify">
+                Incentivizing Users to Participate in Recycling Efforts
+              </p>
+
+            </div>
+          </div>
+
+        </div>
+
+        <div
+          class="tw-shadow-2xl tw-rounded-lg tw-shadow-gray-800 tw-bg-white hover-bg hover:tw-text-white tw-block tw-cursor-pointer tw-w-full">
+
+
+          <div class="tw-px-12 tw-p-4 tw-text-left tw-h-[fit-content] lg:tw-h-full lg:tw-my-8">
+
+
+            <h1 class="tw-font-extrabold tw-text-xl tw-text-black hover:tw-text-white">
+              Circular Economy
+            </h1>
+            <div class="tw-mt-8">
+              <p class="tw-font-medium tw-text-md tw-text-black tw-text-justify hover:tw-text-white">
+                Transforming Plastic Waste into Valuable Resources
+              </p>
+            </div>
+
+
+          </div>
+        </div>
+
+        <div class="tw-shadow-2xl tw-shadow-gray-800 tw-rounded-lg bg-color tw-block tw-cursor-pointer tw-w-full">
+
+          <div class="tw-px-12 tw-p-4 tw-text-left tw-h-[fit-content] lg:tw-h-full lg:tw-my-8">
+            <h1 class="tw-font-extrabold tw-text-xl tw-text-white tw-pt-2">
+              Smart Bin
+            </h1>
+            <div class="tw-mt-8">
+              <p class="tw-font-medium tw-text-md tw-text-white tw-text-justify">
+                Serving as a Plastic Waste Collection Point and Provides Data.
+              </p>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+
+
+
     </div>
 
-    <div class="tw-mt-4 tw-mx-8 lg:tw-mx-0 tw-shadow-2xl tw-rounded-lg tw-shadow-gray-900 lg:tw-px-8 tw-pb-4 tw-aspect-w-16 tw-aspect-h-7 tw-overflow-hidden" data-aos="fade-up">
-
-      <iframe class="tw-shadow-2xl tw-rounded-lg tw-shadow-gray-900"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63800.26059361784!2d30.057428788649723!3d-1.94642533671203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca5a86d814c61%3A0x7d3b83e12b1c11a9!2sNorrsken%20House%20Kigali!5e0!3m2!1sfr!2srw!4v1684802051331!5m2!1sfr!2srw"
-        width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-    </div>
 
   </div>
 </template>
@@ -157,6 +219,32 @@ export default {
         email: "",
         message: "",
       },
+
+      faqItems: [
+        {
+          question: "What is Tailwind CSS?",
+          answer: "Tailwind CSS is a utility-first CSS framework...",
+          expanded: false,
+        },
+        {
+          question: "What is Tailwind CSS?",
+          answer: "Tailwind CSS is a utility-first CSS framework...",
+          expanded: false,
+        },
+        {
+          question: "What is Tailwind CSS?",
+          answer: "Tailwind CSS is a utility-first CSS framework...",
+          expanded: false,
+        },
+        {
+          question: "What is Tailwind CSS?",
+          answer: "Tailwind CSS is a utility-first CSS framework...",
+          expanded: false,
+        },
+        // Add more FAQ items
+      ],
+
+
 
     }
   },
@@ -200,7 +288,9 @@ export default {
 
   methods: {
 
-
+    toggleAnswer(index) {
+      this.faqItems[index].expanded = !this.faqItems[index].expanded;
+    },
     async sendMessage(contact) {
       try {
         const response = await fetch('/api/v1/send-email', {
@@ -254,11 +344,16 @@ export default {
 }
 
 .bg-color {
-  background-color: #03989e;
+  background-color: #0B2447;
 }
 
+:hover.hover-bg {
+  background-color: #0B2447;
+}
+
+
 .text-color {
-  color: #03989e;
+  color: #0B2447;
 }
 
 
